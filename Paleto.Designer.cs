48682d5167beb83982a0a34be3204f7f6304af61
ljiflynn <Lflynn3368@conestogac.on.ca>
2025-01-12 22:14:08 -0500

    Added the chart and data grid controls. Added the logic for loading default data into the chart control on startup.

--- Paleto.Designer.cs
+++ Paleto.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BI_A01
 {
-    partial class Form1
+    partial class Paleto
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.paleto_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.paleto_chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // paleto_chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.paleto_chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.paleto_chart.Legends.Add(legend1);
+            this.paleto_chart.Location = new System.Drawing.Point(49, 63);
+            this.paleto_chart.Name = "paleto_chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Default";
+            this.paleto_chart.Series.Add(series1);
+            this.paleto_chart.Size = new System.Drawing.Size(622, 604);
+            this.paleto_chart.TabIndex = 0;
+            this.paleto_chart.Text = "paleto_chart";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(723, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(633, 326);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Paleto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1389, 756);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.paleto_chart);
+            this.Name = "Paleto";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Paleto_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.paleto_chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart paleto_chart;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Paleto.cs
+++ Paleto.cs
@@ -7,14 +7,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace BI_A01
 {
-    public partial class Form1 : Form
+    public partial class Paleto : Form
     {
-        public Form1()
+        private const int DEFAULT_POINT_COUNT = 10;
+        public Paleto()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Loads data into the pareto chart and data grid on startup
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Paleto_Load(object sender, EventArgs e)
+        {
+            int dataPoint = 100;
+            for (int i = 1; i <= DEFAULT_POINT_COUNT; i++)
+            {
+                dataPoint = dataPoint + (dataPoint / i);
+                paleto_chart.Series["Default"].Points.AddY(dataPoint);
+            }
+
+
+            Update_Data();
+        }
+
+        private void Update_Data()
+        {
+            string chartAreaStr = paleto_chart.Series["Default"].ChartArea;
+            double total = 0.0;
+            double totalPercentage = 0.0;
+            Series series = new Series("Paleto");
+
+            paleto_chart.Series["Default"].ChartType = SeriesChartType.Column;
+            paleto_chart.DataManipulator.Sort(PointSortOrder.Descending, "Default");
+
+            
+            foreach (DataPoint pt in paleto_chart.Series["Default"].Points)
+            {
+                total += pt.YValues[0];
+            }
+            
+            paleto_chart.ChartAreas[chartAreaStr].AxisY.Maximum = total;
+            paleto_chart.Series.Add(series);
+
+            series.ChartType = SeriesChartType.Line;
+            series.BorderWidth = 4;
+            series.ChartArea = paleto_chart.Series["Default"].ChartArea;
+            series.YAxisType = AxisType.Secondary;
+
+            paleto_chart.ChartAreas[chartAreaStr].AxisY2.Maximum = 1;
+            paleto_chart.ChartAreas[chartAreaStr].AxisY2.LabelStyle.Format = "P0";
+            paleto_chart.ChartAreas[chartAreaStr].AxisX.LabelStyle.IsEndLabelVisible = false;
+
+            foreach (DataPoint pt in paleto_chart.Series["Default"].Points)
+            {
+                totalPercentage += (pt.YValues[0] / total);
+                series.Points.Add(Math.Round(totalPercentage, 2));
+            }
         }
     }
 }
